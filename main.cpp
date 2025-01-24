6db647085c66aa13933e66d42e849868cd547a5b
zvdn <130073270+3uvden@users.noreply.github.com>
2025-01-24 13:51:02 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
+#include <iostream>
 #include <numeric>
 
 class Integer
 {
 public:
-	bool isEven(int number)
+	bool isEven(int number)const
 	{
 		if (number % 2 == 0)
 		{
@@ -15,8 +16,7 @@ public:
 			return false;
 		}
 	}
-public:
-	bool isNegative(int number)
+	bool isNegative(int number)const
 	{
 		if (number > 0)
 		{
@@ -27,8 +27,7 @@ public:
 			return false;
 		}
 	}
-public:
-	bool isSimple(int number)
+	bool isSimple(int number)const
 	{
 		if (number % number == 0 && number / 1)
 		{
@@ -39,6 +38,142 @@ public:
 			return false;
 		}
 	}
+	bool isCoprime(Integer other)const
+	{
+		return std::gcd(units_, other.units_) == 1;
+	}
+	int nok(Integer other)
+	{
+		return std::lcm(units_, other.units_);
+	}
+	int nod(Integer other) 
+	{
+		return std::gcd(units_, other.units_);
+	}
+
+
+	 Integer operator+(const Integer& other)const
+	{
+		Integer result;
+		if (sign_ == other.sign_) {
+			result.sign_ = sign_;
+			result.units_ = units_ + other.units_;
+		}
+		else
+		{
+			if (units_ > other.units_)
+			{
+				result.sign_ = sign_;
+				result.units_ = units_ - other.units_;
+			}
+			else if (units_ < other.units_)
+			{
+				result.sign_ = other.sign_;
+				result.units_ = other.units_ - units_;
+			}
+		}
+	}
+
+	 friend Integer operator-(const Integer& a, const Integer& b)
+	 {
+		 Integer result;
+		 if (a.sign_ == b.sign_) {
+			 result.sign_ = a.sign_;
+			 result.units_ = a.units_ - b.units_;
+		 }
+		 else
+		 {
+			 if (a.units_ > b.units_)
+			 {
+				 result.sign_ = a.sign_;
+				 result.units_ = a.units_ - b.units_;
+			 }
+			 else if (a.units_ < b.units_)
+			 {
+				 result.sign_ = b.sign_;
+				 result.units_ = b.units_ - a.units_;
+			 }
+		 }
+	 }
+
+	 friend Integer operator*(const Integer& a, const Integer& b)
+	 {
+		 Integer result;
+		 if (a.sign_ == b.sign_) {
+			 result.sign_ = a.sign_;
+		 }
+		 else
+		 {
+			 if (a.units_ > b.units_)
+			 {
+				 result.sign_ = a.sign_;
+			 }
+			 else if (a.units_ < b.units_)
+			 {
+				 result.sign_ = b.sign_;
+			 }
+		 }
+		 result.units_ = a.units_ * b.units_;
+	 }
+
+	 friend Integer operator/(const Integer& a, const Integer& b)
+	 {
+		 Integer result;
+		 if (a.sign_ == b.sign_) {
+			 result.sign_ = a.sign_;
+			 result.units_ = a.units_ / b.units_;
+		 }
+		 else
+		 {
+			 if (a.units_ > b.units_)
+			 {
+				 result.sign_ = a.sign_;
+				 result.units_ = a.units_ / b.units_;
+			 }
+			 else if (a.units_ < b.units_)
+			 {
+				 result.sign_ = b.sign_;
+				 result.units_ = b.units_ / a.units_;
+			 }
+		 }
+	 }
+
+	 friend Integer operator%(const Integer& a, const Integer& b)
+	 {
+		 Integer result;
+		 if (a.sign_ == b.sign_) {
+			 result.sign_ = a.sign_;
+			 result.units_ = a.units_ % b.units_;
+		 }
+		 else
+		 {
+			 if (a.units_ > b.units_)
+			 {
+				 result.sign_ = a.sign_;
+				 result.units_ = a.units_ % b.units_;
+			 }
+			 else if (a.units_ < b.units_)
+			 {
+				 result.sign_ = b.sign_;
+				 result.units_ = b.units_ % a.units_;
+			 }
+		 }
+	 }
+
+	 Integer& operator+=(Integer other)
+	 {
+
+	 }
+
+private:
+	bool sign_;
+	unsigned int units_;
+};
+
+
+int main()
+{
 	
 
-};
+	return 0;
+}
